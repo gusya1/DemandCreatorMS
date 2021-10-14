b99da0b57e737aef30ca258666fb33bc8fd3d47e
gusya1 <serheos@bk.ru>
2021-10-14 20:05:27 +0300

    add requirements.txt

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+pip3 install --target=venv -r requirements.txt
+
 venv/bin/pyrcc5 -o rc/resources_rc.py resources.qrc
 venv/bin/pyuic5 mainwindow.ui -o ui/ui_mainwindow.py --import-from=rc
-venv/bin/python3.7 -m PyInstaller --onefile --icon pictures\icon.ico -w --paths venv\Lib\site-packages -n DemandCreatorMS main.py
+python3 -m PyInstaller --onefile --icon pictures\icon.ico -w --paths venv\Lib\site-packages -n ProcessingCreatorMS main.py
